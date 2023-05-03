c54cfc9c0186779cce236e507256b7b44f722d47
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-05-03 13:01:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-//your code here
-const prices = document.querySelectorAll("[data-ns-test='prices']");
-let total = 0;
 
-for (let i = 0; i < prices.length; i++) {
-  total += Number(prices[i].textContent);
-}
 
-const grandTotal = document.querySelector("[data-ns-test='grandTotal']");
-grandTotal.textContent = total.toFixed(2);
+//your code here  js
+
+ const table = document.querySelector('table');
+ const lastRow = document.createElement('tr');
+ const prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ const child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+
+
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
